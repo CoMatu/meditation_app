4a0db4668897277b8409542d7c1a527336597b84
CoMatu <30466272+CoMatu@users.noreply.github.com>
2020-04-25 22:47:26 +0500

    card ia ready

--- lib/presentation/widgets/sounds_widget.dart
+++ lib/presentation/widgets/sounds_widget.dart
@@ -1,13 +1,65 @@
 import 'package:flutter/material.dart';
 
+import 'widgets.dart';
+
 class SoundsWidget extends StatelessWidget {
   const SoundsWidget({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Placeholder(
-        color: Colors.amber,
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          HeadLineWidget(),
+          SizedBox(
+            height: 200.0,
+            child: ListView.builder(
+              physics: ClampingScrollPhysics(),
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) =>
+                  MeditationCard(),
+            ),
+          ),
+          HeadLineWidget(),
+          SizedBox(
+            height: 200.0,
+            child: ListView.builder(
+              physics: ClampingScrollPhysics(),
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) =>
+                  MeditationCard(),
+            ),
+          ),
+          HeadLineWidget(),
+          SizedBox(
+            height: 200.0,
+            child: ListView.builder(
+              physics: ClampingScrollPhysics(),
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) =>
+                  MeditationCard(),
+            ),
+          ),
+          HeadLineWidget(),
+          SizedBox(
+            height: 200.0,
+            child: ListView.builder(
+              physics: ClampingScrollPhysics(),
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) =>
+                  MeditationCard(),
+            ),
+          ),
+        ],
       ),
     );
   }
