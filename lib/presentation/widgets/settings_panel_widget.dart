3bd751ac86118bec83e1ee45c0472f3f01e47840
CoMatu <30466272+CoMatu@users.noreply.github.com>
2020-04-26 18:48:10 +0500

    design 99%

--- lib/presentation/widgets/settings_panel_widget.dart
+++ lib/presentation/widgets/settings_panel_widget.dart
@@ -65,21 +65,156 @@ class _SettingPanelWidgetState extends State<SettingPanelWidget> {
           ),
         ),
         Container(
-          child: Row(
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: AssetImage('res/images/background_1.png'),
+          height: 12.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      width: 40.0,
+                      height: 40.0,
+                      decoration: new BoxDecoration(
+                        color: Colors.white,
+                        image: new DecorationImage(
+                          image: new AssetImage('res/images/picture_ocean.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        borderRadius:
+                        new BorderRadius.all(new Radius.circular(20.0)),
+                        border: new Border.all(
+                          color: Colors.white,
+                          width: 1.0,
+                        ),
                       ),
                     ),
-                  )
-                ],
-              ),
-            ],
+                    Container(
+                      height: 4.0,
+                    ),
+                    Text(
+                      'Ocean',
+                      style: TextStyle(color: Colors.white, fontSize: 12.0),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      width: 40.0,
+                      height: 40.0,
+                      decoration: new BoxDecoration(
+                        color: Colors.white,
+                        image: new DecorationImage(
+                          image: new AssetImage('res/images/picture_cafe.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        borderRadius:
+                        new BorderRadius.all(new Radius.circular(20.0)),
+                        border: new Border.all(
+                          color: Colors.white,
+                          width: 1.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 4.0,
+                    ),
+                    Text(
+                      'Cafe',
+                      style: TextStyle(color: Colors.white38, fontSize: 12.0),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      width: 40.0,
+                      height: 40.0,
+                      decoration: new BoxDecoration(
+                        color: Colors.white,
+                        image: new DecorationImage(
+                          image: new AssetImage('res/images/picture_airport.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        borderRadius:
+                        new BorderRadius.all(new Radius.circular(20.0)),
+                        border: new Border.all(
+                          color: Colors.white,
+                          width: 1.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 4.0,
+                    ),
+                    Text(
+                      'Airport',
+                      style: TextStyle(color: Colors.white38, fontSize: 12.0),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      width: 40.0,
+                      height: 40.0,
+                      decoration: new BoxDecoration(
+                        color: Colors.white,
+                        image: new DecorationImage(
+                          image: new AssetImage('res/images/picture_piano.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        borderRadius:
+                        new BorderRadius.all(new Radius.circular(20.0)),
+                        border: new Border.all(
+                          color: Colors.white,
+                          width: 1.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 4.0,
+                    ),
+                    Text(
+                      'Piano',
+                      style: TextStyle(color: Colors.white, fontSize: 12.0),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      width: 40.0,
+                      height: 40.0,
+                      decoration: new BoxDecoration(
+                        color: Colors.white,
+                        image: new DecorationImage(
+                          image: new AssetImage('res/images/picture_storm.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        borderRadius:
+                        new BorderRadius.all(new Radius.circular(20.0)),
+                        border: new Border.all(
+                          color: Colors.white,
+                          width: 1.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 4.0,
+                    ),
+                    Text(
+                      'Storm',
+                      style: TextStyle(color: Colors.white, fontSize: 12.0),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ]),
